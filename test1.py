caed2f99257d89a4ad42cbf5eea6f48e2f95a9bb
setthawutttty <stw.ttty@gmail.com>
2025-12-09 11:25:56 +0700

    test

--- test1.py
+++ test1.py
@@ -9,22 +9,9 @@ from airflow.sdk import dag, task
     tags=["example"],
 )
 def test1():
-    """
-    ### TaskFlow API Tutorial Documentation
-    This is a simple data pipeline example which demonstrates the use of
-    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
-    Documentation that goes along with the Airflow TaskFlow API tutorial is
-    located
-    [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
-    """
+
     @task()
     def extract():
-        """
-        #### Extract task
-        A simple Extract task to get data ready for the rest of the data
-        pipeline. In this case, getting data is simulated by reading from a
-        hardcoded JSON string.
-        """
         data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
 
         order_data_dict = json.loads(data_string)
@@ -32,11 +19,7 @@ def test1():
 
     @task(multiple_outputs=True)
     def transform(order_data_dict: dict):
-        """
-        #### Transform task
-        A simple Transform task which takes in the collection of order data and
-        computes the total order value.
-        """
+
         total_order_value = 0
 
         for value in order_data_dict.values():
@@ -46,11 +29,6 @@ def test1():
 
     @task()
     def load(total_order_value: float):
-        """
-        #### Load task
-        A simple Load task which takes in the result of the Transform task and
-        instead of saving it to end user review, just prints it out.
-        """
 
         print(f"Total order value is: {total_order_value:.2f}")
 
